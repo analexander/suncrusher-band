732f6e3b5b28a03dac581259f67556ba1b5cfda2
Angel Alexander <angale22@gmail.com>
2021-01-31 22:42:52 -0600

    adding nav with back to top scroll button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import AppBar from './components/AppBar'
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    < AppBar />
   );
 }
 

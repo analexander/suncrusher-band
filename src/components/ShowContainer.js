24d393bf1371784997fe2ab028563df3c7290378
Angel Alexander <angale22@gmail.com>
2021-07-12 21:26:35 -0500

    removing past show

--- src/components/ShowContainer.js
+++ src/components/ShowContainer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import Show1 from '../components/Show1';
 import Show2 from '../components/Show2';
 import Show3 from '../components/Show3'
 
@@ -40,12 +39,6 @@ export default function NestedGrid() {
   function FormRow() {
     return (
       <React.Fragment>
-        <Grid item xs={12} sm={9} md={8}>
-          <Typography className={classes.merchName} gutterBottom>
-          Sunday, July 11th 2021 - WC Social Club
-          </Typography>
-          <Show1 />
-        </Grid>
         <Grid item xs={12} sm={9} md={8}>
           <Typography className={classes.merchName} gutterBottom>
             Thursday, August 12th 2021 - Reggies Rock Club
